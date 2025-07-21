3c62ebeb65f0ef58c19f971e2085802a45f04d38
Mohammad Bahmanyar <mbahmnayar72@gmail.com>
2025-07-21 16:04:15 +0330

    Refactor - separate database connection and refactor item retrieval

---
+++ core/Database.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Core;
+
+use PDO;
+
+class Database
+{
+
+    private PDO $connection;
+    private \PDOStatement|false $statement;
+
+    public function __construct()
+    {
+
+        $this->connection = new PDO('mysql:host=localhost;dbname=sarveno', 'root', 'Meb@6057720');
+
+//        $items = $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function query(string $query, ?array $params = null): static
+    {
+        $this->statement = $this->connection->prepare($query);
+        $this->statement->execute($params);
+
+        return $this;
+    }
+
+    public function fetchAll(): array
+    {
+        return $this->statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function fetch(): array
+    {
+        return $this->statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+}
--- index.php
+++ index.php
@@ -6,15 +6,13 @@ require 'vendor/autoload.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-$connection = new PDO('mysql:host=localhost;dbname=sarveno', 'root', 'Meb@6057720');
 
-$statement = $connection->prepare("SELECT * FROM shopping_items");
-$statement->execute();
-
-$items = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 
 header('json: application/json');
+
+$database = new \Core\Database();
+$items= $database->query("SELECT * FROM shopping_items")->fetchAll();
 
 
 
