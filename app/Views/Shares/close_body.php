27670c18945bea3736dedfb04860999280620b71
Mohammad Bahmanyar <mbahmnayar72@gmail.com>
2025-07-27 14:34:31 +0330

    add modal

--- app/Views/Shares/close_body.php
+++ app/Views/Shares/close_body.php
@@ -1,2 +1,17 @@
+<!--<div class="modal-wrapper" id="modal-wrapper">-->
+<!---->
+<!--    <div class="modal-content">-->
+<!--        <h2>modal title</h2>-->
+<!--        <div class="modal-actions">-->
+<!--            <p>Are you sure you want to close this body?</p>-->
+<!--            <div class="modal-buttons">-->
+<!--                <button class="btn btn-secondary" id="close-modal">Close</button>-->
+<!--                <button class="btn btn-primary" id="confirm-close">Confirm</button>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!---->
+<!--</div>-->
+
 </body>
 </html>
